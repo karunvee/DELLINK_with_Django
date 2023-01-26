c28a0bbc5f6c583d8fece241233da2c1e4fcba15
karunvee <you@example.com>
2023-01-26 15:40:18 +0700

    part 1

--- app/forms.py
+++ app/forms.py
@@ -5,3 +5,48 @@ class LineRowForm(forms.ModelForm):
     class Meta:
         model = LineRow
         fields = '__all__'
+        labels = {
+            'plant_name' : '',
+            'line_name' : '',
+            'deviceId' : '',
+            'name' : '',
+            'deviceName' : '',
+            'number' : '',
+            'status' : '',
+            'ip_camera' : '',
+            'picturePath' : '',
+        }
+        widgets = {
+            'plant_name' : forms.TextInput(attrs={
+                'class' : 'input-hidden',
+                'style' : 'display: none;'
+                }),
+            'line_name' : forms.TextInput(attrs={
+                'class' : 'input-hidden',
+                'style' : 'display: none;'
+                }),
+            'deviceId' : forms.TextInput(attrs={
+                'class' : 'input-hidden',
+                'style' : 'display: none;'
+                }),
+            'name' : forms.TextInput(attrs={
+                'class' : 'input-hidden',
+                'style' : 'display: none;'
+                }),
+            'deviceName' : forms.TextInput(attrs={
+                'class' : 'input-hidden',
+                'style' : 'display: none;'
+                }),
+            'number' : forms.TextInput(attrs={
+                'class' : 'input-hidden',
+                'style' : 'display: none;'
+                }),
+            'status' : forms.TextInput(attrs={
+                'class' : 'input-hidden',
+                'style' : 'display: none;'
+                }),
+            'ip_camera' : forms.TextInput(attrs={
+                'class' : 'input-hidden',
+                'style' : 'display: none;'
+                }),
+        }
--- app/urls.py
+++ app/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('line_view/pt<str:pt>ln<str:ln>/', views.line_view, name='line_view'),
     path('machine_view/pt<str:pt>ln<str:ln>mc<str:mc>/', views.machine_view, name='machine_view'),
     path('set_line/pt<str:pt>ln<str:ln>/', views.SetLine, name="set_line"),
+    path('delete_data/pt<str:pt>ln<str:ln>/', views.DeleteData, name="delete_data"),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
